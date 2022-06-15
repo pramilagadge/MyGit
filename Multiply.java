07c1fe7cc152584ea817cf94b53fd382bc310b4a
pgadge <pgadge@xento.com>
2022-06-16 00:23:02 +0530

    Added multiply.java

--- Multiply.java
+++ Multiply.java
@@ -4,8 +4,8 @@
 		public static void main(String[] args) {
 			a = 1;
 			b = 2;
-			sum = a + b;
-		System.out.println("This is sum:"+sum);
+			
+		System.out.println("The product is:"+(a+b));
 
 		}
 
